22b9a046b6e0b0b08ce9354f60da7e0142697663
Miguel Lemos <miguel2015lemos@gmail.com>
2026-05-15 23:30:59 -0300

    "Questões 1,2 e 3"

--- scr/treatment.py
+++ scr/treatment.py
@@ -10,6 +10,23 @@ supabase: Client = create_client(url, key)
 @st.cache_data(ttl=600)
 def carregar_dados():
 
-    resposta = supabase.table("db").select("*").execute()
+    resposta = supabase.table("db").select("*").limit(100000).execute()
     Data = pd.DataFrame(resposta.data)
-print(carregar_dados())
+    return Data
+df=carregar_dados()
+categoria='Office Supplies'
+categorias ='Category'
+cidades = 'City'
+parametro='Sales'
+df_util=df[df[categorias]==categoria]
+
+#AQUI TEMOS A FILA QUE INDICA TODOS OS DADOS DA CIDADE QUE RESPONDE À PERGUNTA 1
+venda_max = df_util[df_util[parametro]==df_util[parametro].max()]
+# AQUI TEMOS A CIDADE EM QUESTÃO JUNTAMENTE COM O SEU ÍNDICE (POSIÇAO NA TABELA GIGANTESCA)
+venda_max[cidades]
+
+datas = df.groupby('Order Date')
+contagem_data=datas.count()
+estados=df.groupby('State')
+contagem_estados=estados.count()
+
