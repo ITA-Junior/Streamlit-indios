d6b6c410facbd16bfc0169faf318a5b27322d79d
Miguel Lemos <miguel2015lemos@gmail.com>
2026-05-21 22:01:24 -0300

    Tornando mais compacto

--- scr/treatment.py
+++ scr/treatment.py
@@ -10,30 +10,22 @@ supabase: Client = create_client(url, key)
 @st.cache_data(ttl=600)
 def carregar_dados():
 
-    resposta = supabase.table("db").select("*").limit(100000).execute()
+    resposta = supabase.table("db").select("*").limit(10000).execute()
     Data = pd.DataFrame(resposta.data)
     return Data
 df=carregar_dados()
-categoria='Office Supplies'
-categorias ='Category'
-cidades = 'City'
-parametro='Sales'
-df_util=df[df[categorias]==categoria]
 
-#AQUI TEMOS A FILA QUE INDICA TODOS OS DADOS DA CIDADE QUE RESPONDE À PERGUNTA 1
-venda_max = df_util[df_util[parametro]==df_util[parametro].max()]
-# AQUI TEMOS A CIDADE EM QUESTÃO JUNTAMENTE COM O SEU ÍNDICE (POSIÇAO NA TABELA GIGANTESCA)
-venda_max[cidades]
-def contagem(object):
-    grupamento=df.groupby(object)
-    return grupamento['Order ID'].count()
+# Separando a Data em dia, mes e ano -
+df['Order Date']=pd.to_datetime(df['Order Date'],format='%Y-%m-%d')
+df['Day']=df["Order Date"].dt.day
+df['Month']=df['Order Date'].dt.month
+df['Year']=df['Order Date'].dt.year
+df['Date']=df['Month'].astype(str)+"//"+df['Year'].astype(str)
 
-datas = df.groupby('Order Date')
-contagem_data=datas["Order ID"].count()
-estados=df.groupby('State')
-contagem_estados=estados["Order ID"].count()
-cidades=df.groupby('City')
-contagem_cidades=cidades["Order ID"].count()
-contagem_cidades=contagem_cidades[0:10]
-segmento=df.groupby("Segment")
-contagem_segmento=segmento['Order ID'].count()
+# Definindo algumas funções
+def agrupar(object):
+    grupo = df.groupby(object)
+    return grupo
+
+def contar(object):
+    return object['Order ID'].count()
