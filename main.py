097519604f821e37a6c2eb86a996d2a5d45a5877
Miguel Lemos <miguel2015lemos@gmail.com>
2026-06-01 14:58:38 -0300

    Começando algumas alterações

--- main.py
+++ main.py
@@ -5,4 +5,17 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 import scr.treatment as tr
 
-st.bar_chart(tr.contar(tr.agrupar(["Year",'Segment'])))
+df = tr.carregar_dados()
+df = tr.formatar(df)
+
+def questao1():
+    data=df[df["Category"]=="Office Supplies"]
+    return data[data['Sales'] == data['Sales'].max()]
+
+
+st.set_page_config(page_title='Dashboard - ITAJr',layout='wide')
+aba1,aba2,aba3,aba4=st.tabs(['Visão Geral','Análises 1','Análises 2','Conclusões'])
+
+with aba1:
+    st.header("Visão Geral")
+    
--- scr/treatment.py
+++ scr/treatment.py
@@ -9,23 +9,16 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 @st.cache_data(ttl=600)
 def carregar_dados():
-
     resposta = supabase.table("db").select("*").limit(10000).execute()
     Data = pd.DataFrame(resposta.data)
     return Data
 df=carregar_dados()
 
-# Separando a Data em dia, mes e ano -
-df['Order Date']=pd.to_datetime(df['Order Date'],format='%Y-%m-%d')
-df['Day']=df["Order Date"].dt.day
-df['Month']=df['Order Date'].dt.month
-df['Year']=df['Order Date'].dt.year
-df['Date']=df['Month'].astype(str)+"//"+df['Year'].astype(str)
+def formatar():
+    df['Order Date']=pd.to_datetime(df['Order Date'],format='%Y-%m-%d')
+    df['Day']=df["Order Date"].dt.day
+    df['Month']=df['Order Date'].dt.month
+    df['Year']=df['Order Date'].dt.year
+    df['Date']=df['Month'].astype(str)+"//"+df['Year'].astype(str)
+    return df
 
-# Definindo algumas funções
-def agrupar(object):
-    grupo = df.groupby(object)
-    return grupo
-
-def contar(object):
-    return object['Order ID'].count()
