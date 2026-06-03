3d3515aeebeeaab0496f49f2a2821f9c02edeb14
vitor-hugo <vhaf22@gmail.com>
2026-06-03 19:13:18 -0300

    ultimas correcoes

--- scr/treatment.py
+++ scr/treatment.py
@@ -4,8 +4,8 @@ import streamlit as st
 from supabase import create_client, Client
 from dotenv import load_dotenv
 load_dotenv()
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = st.secrets["SUPABASE_URL"]
+key: str = st.secrets["SUPABASE_KEY"]
 supabase: Client = create_client(url, key)
 @st.cache_data(ttl=600)
 def carregar_dados():
